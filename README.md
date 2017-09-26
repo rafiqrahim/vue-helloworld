3b9d502ba2e727aea392b294d3b9ef0eaaef22c6
Rafiq Rahim <bizkut@gmail.com>
2017-09-26 17:53:55 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# vue-helloworld
+
+> Basic .vue based project setup
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# run build
+npm run build
+```
+
+For detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var path = require('path')
+
+module.exports = {
+	entry: './src/main.js',
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		filename: 'build.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/
+			},
+			{
+				test: /\.vue$/,
+				loader: 'vue-loader'
+			}
+		]
+	}
+}
